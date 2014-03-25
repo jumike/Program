9d578b49a471d93b538ed42e4dd6d74fc67b5540
13 <13@I13>
2014-03-25 18:53:49 -0500

    este es la primer version

---
+++ src/programa/Programa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package programa;
+
+/**
+ *
+ * @author 13
+ */
+import InterfaceUser.Formulario;
+public class Programa {
+
+    static Formulario i=new Formulario();
+    public static void main(String[] args) {
+        i.setVisible(true);
+    }
+    
+}
